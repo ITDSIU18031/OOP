6670679c4116b2aa64aa231f009cdfccb97919d0
ITDSIU18031 <65488074+ITDSIU18031@users.noreply.github.com>
2020-12-29 10:17:59 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+
+import bookgui.Management.Splash;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ASUS
+ */
+public class Main {
+    public static void  main(String agrs[]){
+        Splash.main(new String[]{});
+    }
+}
